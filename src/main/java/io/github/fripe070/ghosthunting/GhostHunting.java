87a7c596826e3707a12bf145a55ef471180170ab
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-09-26 20:44:14 +0200

    Add an initial version of fluorescent tubes

--- src/main/java/io/github/fripe070/ghosthunting/GhostHunting.java
+++ src/main/java/io/github/fripe070/ghosthunting/GhostHunting.java
@@ -1,13 +1,39 @@
 package io.github.fripe070.ghosthunting;
 
+import io.github.fripe070.ghosthunting.block.FluorescentTubeBlock;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.Identifier;
 
 public class GhostHunting implements ModInitializer {
-    /**
-     * Runs the mod initializer.
-     */
+    public static final String MOD_ID = "ghosthunting";
+
+    public static final Block FLUORESCENT_TUBE = registerBlockWithItem(
+            "fluorescent_tube",
+            new FluorescentTubeBlock(FabricBlockSettings.create()
+            .strength(0.3F)
+            .sounds(BlockSoundGroup.GLASS)
+            .luminance(state -> state.get(Properties.LIT) ? 15 : 0)
+            .pistonBehavior(PistonBehavior.DESTROY)
+    ));
+
+
     @Override
     public void onInitialize() {
 
+    }
+
+    public static <T extends Block> T registerBlockWithItem(String name, T block) {
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, name), block);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, name), new BlockItem(block, new FabricItemSettings()));
+        return block;
     }
 }
---
+++ src/main/java/io/github/fripe070/ghosthunting/block/FluorescentTubeBlock.java
@@ -0,0 +1,9 @@
+package io.github.fripe070.ghosthunting.block;
+
+import net.minecraft.block.RedstoneLampBlock;
+
+public class FluorescentTubeBlock extends RedstoneLampBlock {
+    public FluorescentTubeBlock(Settings settings) {
+        super(settings);
+    }
+}
