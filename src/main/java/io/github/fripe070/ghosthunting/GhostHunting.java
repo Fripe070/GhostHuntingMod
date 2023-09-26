4e3d3c449de0ee666501ca4dc900d9ae70ed1b55
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-09-26 17:00:55 +0200

    Initial commit

---
+++ src/main/java/io/github/fripe070/ghosthunting/GhostHunting.java
@@ -0,0 +1,13 @@
+package io.github.fripe070.ghosthunting;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GhostHunting implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/io/github/fripe070/ghosthunting/client/GhostHuntingClient.java
@@ -0,0 +1,13 @@
+package io.github.fripe070.ghosthunting.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GhostHuntingClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
