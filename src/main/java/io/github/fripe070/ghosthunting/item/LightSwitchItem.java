185e334d56963b916dfb889ebece8f8041cb8fa2
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2023-09-28 20:31:33 +0200

    Refactor LightSwitchItem

--- src/main/java/io/github/fripe070/ghosthunting/item/LightSwitchItem.java
+++ src/main/java/io/github/fripe070/ghosthunting/item/LightSwitchItem.java
@@ -5,50 +5,46 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class LightSwitchItem extends BlockItem {
     public LightSwitchItem(Block block, Settings settings) {
         super(block, settings);
     }
 
-    BlockPos target_block = null;
+    BlockPos targetBlock = null;
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        ActionResult actionResult = null;
-        if (context.getWorld().isClient()) { return ActionResult.PASS; }
-        if (target_block == null) {
-            target_block = context.getBlockPos();
-            context.getWorld().setBlockState(target_block, Blocks.GRAY_CONCRETE.getDefaultState());
+        ActionResult actionResult;
+        World world = context.getWorld();
+        if (world.isClient()) return ActionResult.PASS;
+
+        if (targetBlock == null) {
+            targetBlock = context.getBlockPos();
+            world.setBlockState(targetBlock, Blocks.GRAY_CONCRETE.getDefaultState());
 
             actionResult = ActionResult.SUCCESS;
         } else {
+            // Place lightswitch
             actionResult = super.useOnBlock(context);
 
-            BlockPos clickPos = context.getBlockPos();
+            ItemPlacementContext placementContext = this.getPlacementContext(new ItemPlacementContext(context));
+            if (placementContext == null) return ActionResult.FAIL;
+            BlockEntity lightswitchBlockEntity = world.getBlockEntity(placementContext.getBlockPos());
+            if (!(lightswitchBlockEntity instanceof LightswitchBlockEntity)) return ActionResult.FAIL;
 
-            switch (context.getSide()) {
-                case NORTH -> clickPos = clickPos.north(1);
-                case SOUTH -> clickPos = clickPos.south(1);
-                case EAST -> clickPos = clickPos.east(1);
-                case WEST -> clickPos = clickPos.west(1);
-                case UP -> clickPos = clickPos.up(1);
-                case DOWN -> clickPos = clickPos.down(1);
-            }
-
-            BlockEntity blockEntity = (context.getWorld()).getBlockEntity(clickPos);
-
-            if (blockEntity == null) {
-                return ActionResult.FAIL;
-            }
-
-
-            ((LightswitchBlockEntity) blockEntity).activate_position = new int[]{target_block.getX(), target_block.getY(), target_block.getZ()};
-
-            target_block = null;
+            // Update the nbt data of the lightswitch block entity
+            ((LightswitchBlockEntity) lightswitchBlockEntity).activate_position = new int[]{
+                    targetBlock.getX(),
+                    targetBlock.getY(),
+                    targetBlock.getZ(),
+            };
+            targetBlock = null;
         }
 
         return actionResult;
